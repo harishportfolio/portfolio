dacd13ce0cc600ae7747860519a3992f9f38f6db
harish <atom.harishr@gmail.com>
2019-01-13 19:40:00 +0530

    change s

--- js/custom.js
+++ js/custom.js
@@ -3,65 +3,70 @@ $(document).ready(function() {
         $('.animate-div').hide("slow");
         $('#profile-section').show("slow");
     });
+    $('.footer-icon-home').click(function(){        
+        $('#profile-section').hide("slow");
+        $('.animate-div').show("slow");
+    });
 });
 
 
 Highcharts.chart('container', {
     chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    title: {
-        text: 'Skills'
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        type: 'column'
     },
     exporting: { enabled: false },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                style: {
-                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                }
+    title: {
+        text: ''
+    },    
+    xAxis: {
+        type: 'category',
+        labels: {
+            rotation: -45,
+            style: {
+                fontSize: '12px',
+                fontFamily: 'Verdana, sans-serif'
             }
         }
     },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Percentage'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        pointFormat: '<b>{point.y:.1f}</b>'
+    },
     series: [{
         name: 'Percentage',
-        colorByPoint: true,
-        data: [{
-            name: 'Javascript',
-            y: 20,
-            sliced: true,
-            selected: true
-        }, {
-            name: 'Html',
-            y: 10
-        }, {
-            name: 'Css',
-            y: 10
-        }, {
-            name: 'Php',
-            y: 15
-        }, {
-            name: 'Laravel',
-            y: 20
-        }, {
-            name: 'Angular',
-            y: 15
-        }, {
-            name: 'Node js',
-            y: 10
-        }]
+        data: [
+            ['Html', 50],
+            ['Css', 50],
+            ['Javascript', 60],
+            ['Php', 50],
+            ['Jquery', 60],
+            ['Angular', 60],
+            ['Node js', 50],
+            ['Laravel', 70]
+        ],
+        dataLabels: {
+            enabled: true,
+            rotation: -90,
+            color: '#FFFFFF',
+            align: 'right',
+            format: '{point.y:.1f}', // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+                fontSize: '12px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
     }]
 });
+
 
 function openLinkedIn(){
     window.open('https://www.linkedin.com/in/harish-raveendran/');
