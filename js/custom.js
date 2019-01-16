5767e4fb51c1f95ccaa0021253db20f96fd59e70
harish <atom.harishr@gmail.com>
2019-01-16 20:33:08 +0530

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,17 @@
 
+$('a.page-scroll').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+        }
+    }
+});
+
 // skills chart
 $(document).ready(function (e) {
     var index = 0;
